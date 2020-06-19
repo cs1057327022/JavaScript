4e5553eeb0cf3c2cfffefee684fb97d3c2a37cf8
cs1057327022 <1057327022@qq.com>
2020-06-19 19:19:05 +0800

    全部js

---
+++ 0617js运算符和数据类型/2test.js
@@ -0,0 +1,16 @@
+// for(var i = 0;i<10;i++){
+//     for(var j = 0;j<10;j++){
+//         console.log(i);
+//         console.log(j);
+//     }
+// }
+// 打断调试 watch 监听某一个变量 是否声明 以及赋值的过程
+// 没声明 no available 有声明 初始 undefined 值的变化
+
+var a =  2>1 && 'cs'>'er'&& true+false;
+console.log(a);
+
+var a = '顾坤'
+var b = '刘育才'
+console.log(a.charCodeAt());
+console.log(b.charCodeAt());
